2a52a4d4dc2370ee6b50ad4ce9f1f6c271107916
lgj <2488523491@qq.com>
2024-06-24 20:16:16 +0800

    完成Redis分布式存储答案缓存

---
+++ src/main/java/com/JieAI/AIquestion/scoring/AiTestScoringStrategy.java
@@ -0,0 +1,162 @@
+package com.JieAI.AIquestion.scoring;
+
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.crypto.digest.DigestUtil;
+import cn.hutool.json.JSONUtil;
+import com.JieAI.AIquestion.manager.AiManager;
+import com.JieAI.AIquestion.model.dto.question.QuestionAnswerDTO;
+import com.JieAI.AIquestion.model.dto.question.QuestionContentDTO;
+import com.JieAI.AIquestion.model.entity.App;
+import com.JieAI.AIquestion.model.entity.Question;
+import com.JieAI.AIquestion.model.entity.UserAnswer;
+import com.JieAI.AIquestion.model.vo.QuestionVO;
+import com.JieAI.AIquestion.service.QuestionService;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * AI 测评类应用评分策略
+ */
+@ScoringStrategyConfig(appType = 1, scoringStrategy = 1)
+public class AiTestScoringStrategy implements ScoringStrategy {
+
+    /**
+     * AI 评分系统消息
+     */
+    private static final String AI_TEST_SCORING_SYSTEM_MESSAGE = "你是一位严谨的判题专家，我会给你如下信息：\n" +
+            "```\n" +
+            "应用名称，\n" +
+            "【【【应用描述】】】，\n" +
+            "题目和用户回答的列表：格式为 [{\"title\": \"题目\",\"answer\": \"用户回答\"}]\n" +
+            "```\n" +
+            "\n" +
+            "请你根据上述信息，按照以下步骤来对用户进行评价：\n" +
+            "1. 要求：需要给出一个明确的评价结果，包括评价名称（尽量简短）和评价描述（尽量详细，100到180字）\n" +
+            "2. 严格按照下面的 json 格式输出评价名称和评价描述\n" +
+            "```\n" +
+            "{\"resultName\": \"评价名称\", \"resultDesc\": \"评价描述\"}\n" +
+            "```\n" +
+            "3. 返回格式必须为 JSON 对象";
+    private static String AI_ANSWER_LOCK = "AI_ANSWER_LOCK";
+    /**
+     * AI评分结果缓存
+     */
+    private final Cache<String, String> answerCache =
+            Caffeine
+                    .newBuilder()
+                    //容量最多为1024
+                    .initialCapacity(1024)
+                    //缓存5分钟后移除
+                    .expireAfterAccess(5L, TimeUnit.MINUTES)
+                    .build();
+    @Resource
+    private QuestionService questionService;
+    @Resource
+    private AiManager aiManager;
+
+    @Resource
+    private RedissonClient redissonClient;
+
+    @Override
+    public UserAnswer doScore(List<String> choices, App app) throws Exception {
+
+
+        Long appId = app.getId();
+        String jsonStr = JSONUtil.toJsonStr(choices);
+        String cacheKey = buildCacheKey(appId, jsonStr);
+        String answerJson = answerCache.getIfPresent(cacheKey);
+        if (StrUtil.isNotBlank(answerJson)) {
+            //构造返回值
+            UserAnswer userAnswer = JSONUtil.toBean(answerJson, UserAnswer.class);
+            userAnswer.setAppId(appId);
+            userAnswer.setAppType(app.getAppType());
+            userAnswer.setScoringStrategy(app.getScoringStrategy());
+            userAnswer.setChoices(jsonStr);
+            return userAnswer;
+        }
+        //定义一把锁,不是所有都是一把锁，是相同key的用一把锁
+        RLock lock = redissonClient.getLock(AI_ANSWER_LOCK + cacheKey);
+
+        try {
+            //竞争锁
+            boolean res = lock.tryLock(3, 15, TimeUnit.SECONDS);
+            if(!res){
+                //没抢到锁，强行返回
+                return null;
+            }
+            //抢到锁了执行后续逻辑
+            // 1. 根据 id 查询到题目
+            Question question = questionService.getOne(
+                    Wrappers.lambdaQuery(Question.class).eq(Question::getAppId, appId)
+            );
+            QuestionVO questionVO = QuestionVO.objToVo(question);
+            List<QuestionContentDTO> questionContent = questionVO.getQuestionContent();
+
+            // 2. 调用 AI 获取结果
+            // 封装 Prompt
+            String userMessage = getAiTestScoringUserMessage(app, questionContent, choices);
+            // AI 生成
+            String result = aiManager.doSyncStableRequest(AI_TEST_SCORING_SYSTEM_MESSAGE, userMessage);
+            // 截取需要的 JSON 信息
+            int start = result.indexOf("{");
+            int end = result.lastIndexOf("}");
+            String json = result.substring(start, end + 1);
+
+            //缓存结果
+            answerCache.put(cacheKey, json);
+
+            // 3. 构造返回值，填充答案对象的属性
+            UserAnswer userAnswer = JSONUtil.toBean(json, UserAnswer.class);
+            userAnswer.setAppId(appId);
+            userAnswer.setAppType(app.getAppType());
+            userAnswer.setScoringStrategy(app.getScoringStrategy());
+            userAnswer.setChoices(JSONUtil.toJsonStr(choices));
+            return userAnswer;
+        }finally {
+            if(lock != null && lock.isLocked()){
+                if(lock.isHeldByCurrentThread()){
+
+                }
+            }
+        }
+    }
+
+    /**
+     * AI 评分用户消息封装
+     *
+     * @param app
+     * @param questionContentDTOList
+     * @param choices
+     * @return
+     */
+    private String getAiTestScoringUserMessage(App app, List<QuestionContentDTO> questionContentDTOList, List<String> choices) {
+        StringBuilder userMessage = new StringBuilder();
+        userMessage.append(app.getAppName()).append("\n");
+        userMessage.append(app.getAppDesc()).append("\n");
+        List<QuestionAnswerDTO> questionAnswerDTOList = new ArrayList<>();
+        for (int i = 0; i < questionContentDTOList.size(); i++) {
+            QuestionAnswerDTO questionAnswerDTO = new QuestionAnswerDTO();
+            questionAnswerDTO.setTitle(questionContentDTOList.get(i).getTitle());
+            questionAnswerDTO.setUserAnswer(choices.get(i));
+            questionAnswerDTOList.add(questionAnswerDTO);
+        }
+        userMessage.append(JSONUtil.toJsonStr(questionAnswerDTOList));
+        return userMessage.toString();
+    }
+
+    /**
+     * 构建缓存key
+     */
+    private String buildCacheKey(Long appId, String choices) {
+        return DigestUtil.md5Hex(appId + ":" + choices);
+    }
+
+}
